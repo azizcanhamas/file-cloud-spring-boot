cf7374f2796d4576b710daa22f88607bc08c2134
Aziz Can HAMASOGLU <azizcanhamas@gmail.com>
2023-07-27 18:58:09 +0300

    UPDATE

--- reports/Gun_13_29.03.2023/h2example/h2example/src/main/java/com/h2group/h2example/CitizenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.h2group.h2example;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CitizenRepository extends JpaRepository<Citizen, Long> {
-    List<Citizen> findByName(String name);
-}
--- reports/Gun_7_21.03.2023/user/src/main/java/com/users/user/CitizenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.users.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CitizenRepository extends CrudRepository<Citizen, Integer> {
-}
